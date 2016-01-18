3858163c67548854c58e45cc621481ca4fa05fef
LDelhez <louisdelhez@yahoo.fr>
2016-01-18 23:09:58 +0100

    Update prog_best.c

--- Janvier-2012/Q4/prog_best.c
+++ Janvier-2012/Q4/prog_best.c
@@ -5,7 +5,11 @@
 int longueur_prefixe_suffixe(int *t, int n) {
   int i;
   int prev = -1;
-  for (i=1;i<n;i++){
+  /*
+   * Invariant:
+   * prev contient la longueur de la plus grande sous-chaine préfixe/suffixe de t[0..i-1]
+   */
+  for (i=1;i<n;i++){ 
     if (prev <= 0 && t[0] == t[i])
       prev = 1;
     else if (prev >= 1 && t[prev] == t[i])
